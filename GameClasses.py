16a68d6e56b9db9b41f5838ba427f74764101cfe
IoflyTang <58381062+IoflyTang@users.noreply.github.com>
2019-12-10 23:15:21 +0800

    ty: 数据结构都对象化，现在可以支持多局游戏

--- GameClasses.py
+++ GameClasses.py
@@ -1,10 +1,51 @@
 import math
-import Global
+from queue import Queue
+
 import random
+
+class GlobalInfo:
+    time = 0
+    length = 1
+    poison = 0.1
+    tank_v = 0.1
+    # tank_size = 1
+    ammo_v = 0.1
+    ammo_attack = 10
+    # circle_v = 1
+    ammo_num = 0
+
+    item_hp = 10
+    item_ammo = 10
+
+    current_x1 = 1
+    current_y1 = 2
+    current_x2 = 3
+    current_y2 = 4
+
+    brick_list = []
+    item_list = []
+
+    operate_queue = Queue()
+    operate_dict = {}
+
+    ammo_list = []
+    tank_list = []
+    brick_changed = []
+    item_changed = []
+
+    tanks = []
+    bulls = []
+    obs = []
+    glass = []
+    props = []
+
+
+    output_dict = {'tank': tank_list, 'ammo': ammo_list, 'brick': brick_changed, 'item': item_changed}
+
 
 class Tank:
 
-    def __init__(self, tank_id, hp, ammo, kill, x, y, direction):
+    def __init__(self, tank_id, hp, ammo, kill, x, y, direction, info):
         self.tank_id = tank_id
         self.hp = hp
         self.ammo = ammo
@@ -12,42 +53,43 @@ class Tank:
         self.x = x
         self.y = y
         self.direction = direction 
+        self.info = info
 
     def shoot(self, fire):
         if fire==1:
             self.ammo = self.ammo - 1
-            Global.ammo_num = Global.ammo_num + 1
-            Global.ammo_list.append(Ammo(self.tank_id, Global.ammo_num, self.x, self.y, self.direction))
+            self.info.ammo_num = self.info.ammo_num + 1
+            self.info.ammo_list.append(Ammo(self.tank_id, self.info.ammo_num, self.x, self.y, self.direction, self.info))
 
 
     def drive(self, up, down, left, right):
         if not self.is_in_circle():
-            self.hp = self.hp - Global.poison
+            self.hp = self.hp - self.info.poison
 
         self.direction = self.direction + (right - left)
-        temp_x = self.x + math.sin(self.direction) * Global.tank_v*(up-down)
-        temp_y = self.y - math.cos(self.direction) * Global.tank_v*(up-down)
-        for tank in Global.tank_list:
+        temp_x = self.x + math.sin(self.direction) * self.info.tank_v*(up-down)
+        temp_y = self.y - math.cos(self.direction) * self.info.tank_v*(up-down)
+        for tank in self.info.tank_list:
             if 0.1<(temp_x - tank.x) * (temp_x - tank.x) + (temp_y - tank.y) * (temp_y - tank.y) <= 1.0:
                 return
-        for brick in Global.brick_list:
+        for brick in self.info.brick_list:
             if (-0.5 <= temp_x-brick.x <=1.5 )&(-0.5 <= temp_y-brick.y <=1.5):
                 return
 
         self.x = temp_x
         self.y = temp_y
-        for item in Global.item_list:
+        for item in self.info.item_list:
             if (-0.5 <= self.x - item.x <= 1.5) & (-0.5 <= self.y - item.y <= 1.5):
                 item.disappear()
-                Global.item_changed.append(item)
+                self.info.item_changed.append(item)
                 if item.type_id ==0:
-                    tank.hp = tank.hp + Global.item_hp
+                    tank.hp = tank.hp + self.info.item_hp
                 elif  item.type_id ==1:
-                    tank.ammo = tank.ammo+Global.item_ammo
+                    tank.ammo = tank.ammo+self.info.item_ammo
 
 
     def is_in_circle(self):
-        if (Global.current_x1<self.x<Global.current_x2)and(Global.current_y1<self.y<Global.current_y2):
+        if (self.info.current_x1<self.x<self.info.current_x2)and(self.info.current_y1<self.y<self.info.current_y2):
             return True
         else:
             return False
@@ -55,29 +97,33 @@ class Tank:
 
 class Ammo:
 
-    def __init__(self, tank_id, ammo_id, x, y, direction):
+    def __init__(self, tank_id, ammo_id, x, y, direction, info):
         self.tank_id = tank_id
         self.ammo_id = ammo_id
         self.x = x
         self.y = y
         self.direction = direction
         self.exist = 1
-
+        self.info = info
     def move(self):
-        self.x = self.x + math.sin(self.direction) * Global.ammo_v
-        self.y = self.y - math.cos(self.direction) * Global.ammo_v
+        self.x = self.x + math.sin(self.direction) * self.info.ammo_v
+        self.y = self.y - math.cos(self.direction) * self.info.ammo_v
 
     def refresh(self):
-        for tank in Global.tank_list:
-            if (self.x-tank.x)*(self.x-tank.x)+(self.y-tank.y)*(self.y-tank.y) <= 0.25:
-                tank.hp = tank.hp - Global.ammo_attack
+        for tank in self.info.tank_list:
+            if (self.x-tank.x)*(self.x-tank.x)+(self.y-tank.y)*(self.y-tank.y) <= 0.25 and self.tank_id != tank.tank_id:
+                tank.hp = tank.hp - self.info.ammo_attack
                 self.exist = 0
+                if tank.hp <= 0:
+                    for t in self.info.tank_list:
+                        if t.tank_id == self.tank_id:
+                            t.kill = t.kill + 1   #这里可以加上杀人信息
 
-        for brick in Global.brick_list:
+        for brick in self.info.brick_list:
             if (0 < self.x-brick.x <=1 )&(0 < self.y-brick.y <=1):
                 self.exist = 0
                 brick.disappear()
-                Global.brick_changed.append(brick)
+                self.info.brick_changed.append(brick)
 
 
 
@@ -115,30 +161,30 @@ class Circle:
 
     def __init__(self,
                  current_x1, current_y1, current_x2, current_y2,
-                 target_x1, target_y1, target_x2, target_y2):
+                 target_x1, target_y1, target_x2, target_y2,  info):
         self.current_x1 = current_x1
         self.current_y1 = current_y1
         self.current_x2 = current_x2
         self.current_y2 = current_y2
-        Global.current_x1 = current_x1
-        Global.current_y1 = current_y1
-        Global.current_x2 = current_x2
-        Global.current_y2 = current_y2
+        self.info.current_x1 = current_x1
+        self.info.current_y1 = current_y1
+        self.info.current_x2 = current_x2
+        self.info.current_y2 = current_y2
         self.target_x1 = target_x1
         self.target_y1 = target_y1
         self.target_x2 = target_x2
         self.target_y2 = target_y2
-
+        self.info = info
     def refresh(self):
-        if Global.time%10==0:
+        if self.info.time%10==0:
             self.current_x1 = self.current_x1 + int(self.current_x1 < self.target_x1)
             self.current_y1 = self.current_y1 + int(self.current_y1 < self.target_y1)
             self.current_x2 = self.current_x2 + int(self.current_x2 < self.target_x2)
             self.current_y2 = self.current_y2 + int(self.current_y2 < self.target_y2)
-            Global.current_x1 =  self.current_x1
-            Global.current_y1 = self.current_y1
-            Global.current_x2 = self.current_x2
-            Global.current_y2 = self.current_y2
+            self.info.current_x1 =  self.current_x1
+            self.info.current_y1 = self.current_y1
+            self.info.current_x2 = self.current_x2
+            self.info.current_y2 = self.current_y2
 
         if (self.current_x1 == self.target_x1)and(self.current_y1 == self.target_y1)and(self.current_x2 == self.target_x2)and(self.current_y2 ==self.target_y2):
             self.target_x1= self.target_x1 + random.randint(0, self.target_x2-self.target_x1)
--- GameCore.py
+++ GameCore.py
@@ -1,67 +1,67 @@
 from GameClasses import *
-import Global
 import random
 
 
-circle = Circle(8,7,6,5,4,3,2,1)
+class GameCore:
+    def __init__(self, num):
+        self.num = num
+        self.info = GlobalInfo()
+        self.circle = Circle(8, 7, 6, 5, 4, 3, 2, 1, self.info)
 
+    def game_init(self):
+        pass
 
-def game_init():
-    pass
+    def input_data(self,data):
+        self.info.operate_queue.put(data)
 
-def input_data(data):
-    Global.operate_queue.put(data)
+    def output_data(self):
+        return self.info.output_dict
 
+    def item_refresh(self):
+        if self.info.time % 60 == 0:
+            for i in range(len(self.info.tank_list)):
+                x = random.randint(self.circle.current_x1, self.circle.current_x2)
+                y = random.randint(self.circle.current_y1, self.circle.current_y2)
+                type_id = random.randint(0, 1)
+                self.info.item_changed.append(Item(x, y, type_id))
+                x = random.randint(self.circle.current_x1, self.circle.current_x2)
+                y = random.randint(self.circle.current_y1, self.circle.current_y2)
+                type_id = random.randint(0, 1)
+                self.info.item_changed.append(Item(x, y, type_id))
 
-def output_data():
-    return Global.output_dict
+    def is_over(self):
+        if len(self.info.tank_list) == 1:
+            return True
+        else:
+            return False
 
-def item_refresh():
-    if Global.time%60 ==0:
-        for i in range(len(Global.tank_list)):
-            x = random.randint(circle.current_x1, circle.current_x2)
-            y = random.randint(circle.current_y1, circle.current_y2)
-            type_id = random.randint(0, 1)
-            Global.item_changed.append(Item(x, y, type_id))
-            x = random.randint(circle.current_x1, circle.current_x2)
-            y = random.randint(circle.current_y1, circle.current_y2)
-            type_id = random.randint(0, 1)
-            Global.item_changed.append(Item(x, y, type_id))
+    def gaming(self):
+        # operation_num = operate_queue.qsize()
 
-def is_over():
-    if len(Global.tank_list) == 1:
-        return True
-    else:
-        return False
+        while not self.info.operate_queue.empty():
+            operate = self.info.operate_queue.get()
 
+            tank_id = operate[0]
+            up = operate[1]
+            down = operate[2]
+            left = operate[3]
+            right = operate[4]
+            fire = operate[5]
+            tank = self.info.tank_list[tank_id]
+            tank.shoot(fire)
+            tank.drive(up, down, left, right)
 
-def gaming():
-    #operation_num = operate_queue.qsize()
+            for ammo in self.info.ammo_list:
+                ammo.move()
+                ammo.refresh()
 
-    while not Global.operate_queue.empty():
-        operate = Global.operate_queue.get()
+            self.circle.refresh()
+            self.item_refresh()
 
-        tank_id = operate[0]
-        up = operate[1]
-        down = operate[2]
-        left = operate[3]
-        right = operate[4]
-        fire = operate[5]
-        tank = Global.tank_list[tank_id]
-        tank.shoot(fire)
-        tank.drive(up, down, left, right)
-
-        for ammo in Global.ammo_list:
-            ammo.move()
-            ammo.refresh()
-
-        circle.refresh()
-        item_refresh()
-
-        Global.output_dict['tank'] = Global.tank_list
-        Global.output_dict['ammo'] = Global.ammo_list
-        Global.output_dict['brick'] = Global.brick_changed
-        Global.output_dict['item'] = Global.item_changed
+            self.info.output_dict['tank'] = self.info.tank_list
+            self.info.output_dict['ammo'] = self.info.ammo_list
+            self.info.output_dict['brick'] = self.info.brick_changed
+            self.info.output_dict['item'] = self.info.item_changed
 
 
 
